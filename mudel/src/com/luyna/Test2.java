cc97abec9b7bc82720adf66e6173b1e05116fbc4
luyna <yuluan1991@gmail.com>
2016-07-17 18:00:58 +0800

    第二次提交

--- mudel/src/com/luyna/Test2.java
+++ mudel/src/com/luyna/Test2.java
@@ -3,16 +3,15 @@ package com.luyna;
 /**
  * Created by Luyna on 2016/7/17.
  */
-public class Test1 {
+public class Test2 {
     private String name;
 
-    public Test1(String name) {
+    public Test2(String name) {
         this.name = name;
     }
 
     public static void main(String[] args) {
-        Test1 ha = new Test1("luyna");
+        Test2 ha = new Test2("luyna");
         System.out.println(ha.toString());
-        System.out.println("hhahah");
     }
 }
