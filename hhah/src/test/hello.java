4eeabe169f9ad07986383486ed19b6cced81ae65
luyna <yuluan1991@gmail.com>
2016-07-17 19:54:00 +0800

    stash以后修改内容1

--- hhah/src/test/hello.java
+++ hhah/src/test/hello.java
@@ -7,5 +7,7 @@ public class hello {
     public static void main(String[] args){
         System.out.println("ffdffffffff");
         System.out.println("stash 以后修改。。。");
+        System.out.println("stash....");
+        System.out.println("stash test1");
     }
 }
