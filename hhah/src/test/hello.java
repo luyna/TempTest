3baba8ada1e5b8d25c873bf7d9973336ee9625ed
luyna <yuluan1991@gmail.com>
2016-07-17 19:23:28 +0800

    dev1 commit

---
+++ hhah/src/test/hello.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Luyna on 2016/7/17.
+ */
+public class hello {
+    public static void main(String[] args){
+        System.out.println("ffdffffffff");
+    }
+}
