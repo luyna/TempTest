08239e60fb4455f1327960d0ed9f2a1fc5ec4ed8
luyna <yuluan1991@gmail.com>
2016-07-17 19:18:41 +0800

    Merge branch 'master' of E:\Github\TempTest with conflicts.

---
+++ mudel/src/com/luyna/Test2.java
@@ -0,0 +1,18 @@
+package com.luyna;
+
+/**
+ * Created by Luyna on 2016/7/17.
+ */
+public class Test2 {
+    private String name;
+
+    public Test2(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Test2 ha = new Test2("luyna");
+        System.out.println(ha.toString());
+        System.out.println("aaaaaaaaaaa");
+    }
+}
